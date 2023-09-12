ce1ab851b1a3f0e2258c8e35a5af748d09d03ccd
huichenguomin <120192759+huichenguomin@users.noreply.github.com>
2023-09-12 10:28:18 +0800

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,9 @@
+using System;
+namespace HelloWorld{
+    class HelloWorld{
+        static void main(string[] args){
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
